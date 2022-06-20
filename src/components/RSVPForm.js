6aa231187c84fb04d4228e3a3ff834c3ceb65ab4
devandapaige <amanda.gaylord@gmail.com>
2022-06-19 21:01:29 -0400

    reformat for just frontend

---
+++ src/components/RSVPForm.js
@@ -0,0 +1,7 @@
+import { Widget } from '@typeform/embed-react'
+
+const RSVPForm = (props) => {
+    return <Widget id={props.rsvpFormID} style={{width: '55%'}} className="typeform" />
+}
+
+export default RSVPForm
